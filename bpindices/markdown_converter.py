ddad9f7678a99f2ad2bce57f1ace8cdab402d73d
aniabuto <aniabuto@wp.pl>
2023-07-06 21:51:03 +0200

    added saving markdown to file and generating shorty description text

--- bpindices/markdown_converter.py
+++ bpindices/markdown_converter.py
@@ -34,8 +34,8 @@ def dataFrameToMarkdown(df):
     markdown_table = "\n".join(markdown_rows)
     return markdown_table
 
-def createMdFromDataFrame(json_file, metric):
-    df = extractSingleMetricToDataFrame(metric, parseTableToDataFrame(json_file))
+def createMdTableFromDataFrame(data_frame, metric):
+    df = extractSingleMetricToDataFrame(metric, data_frame)
     max_values = df.max()
     markdown = dataFrameToMarkdown(df)
 
@@ -58,5 +58,44 @@ def createMdFromDataFrame(json_file, metric):
     return new_markdown
 
 
-# parseTableToDataFrame("params.json")
-print(createMdFromDataFrame("params.json", "mean"))
+def createDescriptionForMetric(data_frame, metric):
+    df = extractSingleMetricToDataFrame(metric, data_frame)
+    max_values = df.max()
+
+    markdown = dataFrameToMarkdown(df)
+    markdown_rows = markdown.split('\n')
+
+    ids = ["", "", ""]
+
+    title = 0
+    for row in markdown_rows:
+        split_row = row.split('|')[1:-1]
+        for i in range(0, len(split_row)):
+            if i >= len(split_row) - 3 and title > 1:
+                value = float(split_row[i])
+                if max_values[i] == value:
+                    ids[i - (len(split_row) - 3)] = (split_row[0], value)
+                    # split_row[i] = "**" + split_row[i].strip() + "**"
+        title += 1
+
+    return "After calculating " + metric + " for each patient's systolic, diastolic and average " \
+           "blood pressure we received following results (see table below). As we can see, highest " \
+           "systolic blood pressure was detected for patient " + str(ids[0][0]) + " and reached " + str(ids[0][1]) +\
+           ". Highest diastolic blood pressure was detected for patient " + str(ids[1][0]) + " and reached " + str(ids[1][1]) +\
+           ". Highest average blood pressure was detected for patient " + str(ids[2][0]) + " and reached " + str(ids[2][1]) + "."
+
+
+def createMarkdownForMetric(data_frame, metric):
+    markdown_text = createDescriptionForMetric(data_frame, metric)
+    markdown_table = createMdTableFromDataFrame(data_frame, metric)
+
+    return markdown_text + "\n\n\n" + markdown_table
+
+
+def writeMarkdownForMetricToFile(data_frame, metric, filename):
+    with open(filename, "w") as file:
+        file.write(createMarkdownForMetric(data_frame, metric))
+
+
+
+writeMarkdownForMetricToFile(parseTableToDataFrame("params.json"), "entropy", "entropy.md")
