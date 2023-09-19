e54a3a801cbe1b6ffb35499c0606f2aaeb98c94d
aniabuto <aniabuto@wp.pl>
2023-09-19 13:14:52 +0200

    added dimension reduction to correlation

--- src/analyzers/correlation.py
+++ src/analyzers/correlation.py
@@ -5,6 +5,9 @@ import seaborn
 import src.bpvappcontext as appctx
 import src.gui.forminputs as forminputs
 from src.analyzers.abstractanalyzer import AbstractAnalyzer
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import umap
 from src.markdownoutput import MarkdownOutput
 
 FIGNUM_CORRELATION = 1
@@ -19,6 +22,25 @@ class CorrelationAnalyzer(AbstractAnalyzer):
                 key="mode",
                 choices=["pearson", "spearman"],
                 default_choice_str="pearson"
+            ),
+            forminputs.Text(
+                key="file_name",
+                initial_value="correlation"
+            ),
+            forminputs.Boolean(
+                key="reduce_dimensions",
+                initial_value=False
+            ),
+            forminputs.OneOf(
+                key="method_to_reduce_dimensions",
+                choices=["UMAP", "t-SNE", "PCA"],
+                default_choice_str="PCA"
+            ),
+            forminputs.Number(
+                key="number_of_dimensions",
+                min_value=2,
+                max_value=len(ctx.get_selected_index_paths()),
+                initial_value=2
             )
         ]
 
@@ -29,7 +51,19 @@ class CorrelationAnalyzer(AbstractAnalyzer):
         pass
 
     def process(self, active_dataframe: pandas.DataFrame):
-        self.corr_mtx = active_dataframe.corr(method=self.config["mode"])
+        arr = active_dataframe
+        if self.config["reduce_dimensions"]:
+            if self.config["method_to_reduce_dimensions"] == "UMAP":
+                reducer = umap.UMAP(n_components=self.config["number_of_dimensions"])
+                arr = reducer.fit_transform(arr)
+            elif self.config["method_to_reduce_dimensions"] == "t-SNE":
+                arr = TSNE(n_components=self.config["number_of_dimensions"], perplexity=30).fit_transform(arr)
+            else:
+                arr = PCA(n_components=self.config["number_of_dimensions"]).fit_transform(
+                    arr)  # transform data frame to 2D
+            arr = pandas.DataFrame(data=arr, columns=[str(x) for x in range(self.config["number_of_dimensions"])])
+
+        self.corr_mtx = arr.corr(method=self.config["mode"])
 
     def present(self):
         plt.figure(FIGNUM_CORRELATION)
